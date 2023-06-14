a57f19bb86a3e4983d2a8d0e98025144aa1c609b
agatha-d <agatha.duranceau@epfl.ch>
2023-06-14 15:44:06 +0200

    edymobiles can be controlled, still problens yith changes in direction

---
+++ src/agatha_generate_goals.py
@@ -0,0 +1,542 @@
+# Difference dans la generation des taches, on ne peut pas avoir deux fois le meme goal
+# On envoie les goals aux robots
+# Structure finale
+# On effectue les missions de A à Z 
+# on envoie des checkpoints aux robots - changement de direction
+# on calcul une seule fois le chemin
+
+# python3 run_experiments.py --disjoint --instance instances/circuit.txt --solver Prioritized
+
+
+# Librairie
+from pathlib import Path
+from single_agent_planner import compute_heuristics, a_star, get_sum_of_cost
+import random
+import numpy as np
+from hungarian_algorithm import hungarian_algorithm, ans_calculation
+from munkres import Munkres # hungarian algorithm
+from cbs import CBSSolver
+#from prioritized import PrioritizedPlanningSolver
+
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import *
+from tf.msg import *
+import configparser
+
+
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+#stations = { 'discovery' : (5, 2), 'omni' : (5, 10), 'synt' : (9, 10), 'sfc' : (0, 10) }
+#robots = { '0' : (17, 1), '1' : (19, 1), '2' : (3, 5), '3' : (17, 10),'4' : (19, 10)}
+#number_robot = 5
+#number_station = 4
+#state_var = 0 # 0: free, 1: has a task
+
+EPSILON = 0.0001
+
+class Robot():
+
+    def __init__(self, nameRobot, initX, initY, number):
+        self.name = nameRobot
+        self.id = number
+        self.posX = initX
+        self.posY = initY
+        self.goalX = [initX]
+        self.goalY = [initY]
+        self.path = []
+        self.produit = np.array([[0, 1, -3.6],[-1, 0, 4.5], [0, 0, 1]]) @ (self.posX/2.1, self.posY/2.1, 1)
+        self.posX_gazebo = round(self.produit[0], 2 )
+        self.posY_gazebo = round(self.produit[1], 2 )
+        self.goal_tempX = 0
+        self.goal_tempY = 0
+        self.path_gazebo = []
+        self.state_checkpoint = 0 # 0: arrived, 1: on the way
+        self.state = 0 # 0: free, 1: has a task
+        #self.checkpoint = []
+
+        # ROS Publisher for sending goal to controller
+        self.pub = rospy.Publisher('/' +self.name+ '/move_base/goal',MoveBaseGoal, queue_size=10)
+
+
+    def Position(self, odom_data):
+        curr_time = odom_data.header.stamp
+        self.posX_gazebo = round(odom_data.pose.pose.position.x, 2)
+        self.posY_gazebo = round(odom_data.pose.pose.position.y, 2)
+
+
+    def base_MAPF2Gazebo(self):
+        '''
+        Change of base MAPF 2 GAZEBO
+        - input: paths in the MAPF frame
+        - output: paths in the GAZEBO frame
+        '''
+        self.path_gazebo.clear()
+        matrix_chg_base = np.array([[0, 1, -3.6],[-1, 0, 4.5], [0, 0, 1]])
+        #print(self.name, len(self.path), self.path)
+        for i in range(0, len(self.path)): #We start at 1 so we don't take into account the initial position NOT the case anymore
+            path_temp = (self.path[i][0]/2.1, self.path[i][1]/2.1, 1)
+            produit = matrix_chg_base @ path_temp
+            self.path_gazebo.append((round(produit[0], 2), round(produit[1], 2)))
+        print('Path in converted Gazebo coordinate')
+
+
+    def base_Gazebo2MAPF(self):
+        '''
+        Change of base GAZEBO 2 MAPF
+        - input: paths in the MAPF frame
+        - output: paths in the GAZEBO frame
+        '''
+        matrix_chg_base = np.array([[0, -1, 4.5],[1, 0, 3.6], [0, 0, 1]])
+        path_temp = (self.posX_gazebo, self.posY_gazebo, 1)
+        produit = matrix_chg_base @ path_temp
+        self.posX = int(round(produit[0]*2.1, 0))
+        self.posY = int(round(produit[1]*2.1, 0))
+        if self.posX > 19:
+            self.posX = 19
+        if self.posY > 12:
+            self.posX = 12
+
+
+    def send_goal(self):
+        '''
+        Publish robot next goal or checkpoint
+        '''
+        print('----SEND GOAL----')
+
+        # Set up the goal
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.pose.orientation.w = 1.0 # not used
+        self.state_checkpoint = 1
+        
+        if len(self.path_gazebo)> 1:
+            self.path_gazebo.pop(0)
+            goal.target_pose.pose.position.x = self.path_gazebo[0][0]
+            goal.target_pose.pose.position.y = self.path_gazebo[0][1]
+            self.goal_tempX = self.path_gazebo[0][0]
+            self.goal_tempY = self.path_gazebo[0][1]
+            print('new checkpoint or goal for ', self.name, self.path_gazebo[0][0], self.path_gazebo[0][1])
+            
+        else:
+            goal.target_pose.pose.position.x = self.path_gazebo[0][0]
+            goal.target_pose.pose.position.y = self.path_gazebo[0][1]
+            print('same pos', self.path_gazebo[0][0], self.path_gazebo[0][1])
+            self.state = 0 # the robot is free again
+
+        # Publish the goal and 
+        self.pub.publish(goal)
+
+        #TODO: wait for completion of the turn in change dir ??
+
+        rospy.loginfo('Goal'+str(goal))
+
+        return(goal)
+    
+    
+    # TODO: remove, probably unnecessary
+    def change_direction(self):
+        '''
+        Only keep changes of direction in the path
+        '''
+        self.checkpoint.clear()
+        print('path', self.path, len(self.path))
+        for i in range(0, len(self.path)-1):
+            if len(self.path) == 1:
+                self.checkpoint.append(self.path[0])
+            else:
+                if self.path[i][0] != self.path[i+1][0] or self.path[i][1] != self.path[i+1][1]:
+                    print('here')
+                    self.checkpoint.append(self.path[i])    
+        print('checkpoint', self.checkpoint)
+    
+    
+class Station:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+
+
+def import_mapf_instance(filename):
+    '''
+    MAP GENERATION (FOR A*)
+    - input: text file representing the map
+    - output: table containing free and occupied cells
+    '''
+    f = Path(filename)
+    if not f.is_file():
+        raise BaseException(filename + " does not exist.")
+    f = open(filename, 'r')
+    # first line: #rows #columns
+    line = f.readline()
+    rows, columns = [int(x) for x in line.split(' ')]
+    rows = int(rows)
+    columns = int(columns)
+    # #rows lines with the map
+    my_map = []
+    for r in range(rows):
+        line = f.readline()
+        my_map.append([])
+        for cell in line:
+            if cell == '@':
+                my_map[-1].append(True)
+            elif cell == '.':
+                my_map[-1].append(False)
+    print('Map generated')
+    return my_map
+
+
+def task_generator(stations):
+    '''
+    TASK GENERATION: generate random tasks for the robots
+    TODO: get tasks from a task generation manager at higher level
+    - input: stations: discovery,5,2;omni,5,10;synt,9,10;sfc,0,10
+    '''
+    #tasks = [[[0,3],[1,2]], [[1,2],[0,3]], [[1,0],[3,2]], [[0,3, 1],[1,2, 0]], [[3, 1,2],[1, 0,3]], [[2, 1,0],[0,3,2]], [[3, 2, 1,0],[1,0,3,2]]]
+    #tasks_to_do = random.choice(tasks)
+    tasks_to_do = [[0,3],[1,2]]
+   
+    return tasks_to_do[0], tasks_to_do[1]
+
+
+def task_allocation(stations, tasks_to_do, next_tasks_to_do):
+    '''
+    TASK ALLOCATION
+    - input: robots
+    - output: goals allocated to each robot
+    '''
+
+    starts_robots = [(robot.posX, robot.posY) for robot in robots]
+    goals_allocation = []
+    cost_matrix = np.zeros((len(tasks_to_do), len(robots)))
+    for i in range(0, len(tasks_to_do)):
+        goals_allocation.append((stations[tasks_to_do[i]].x, stations[tasks_to_do[i]].y))
+    
+    heuristics = []
+    for goal in goals_allocation:
+        heuristics.append(compute_heuristics(my_map, goal))
+
+    # compute the cost matrix
+    for i in range(0, len(tasks_to_do)):
+        for j in range(0, len(robots)):
+            path = a_star(my_map, starts_robots[j], goals_allocation[i], heuristics[i], 0, [])
+            cost_matrix[i][j]=len(path)
+
+    # Hungarian algorithm
+    m = Munkres()
+    indexes = m.compute(cost_matrix) # it outputs a list of assignments (task, robot)
+
+    print(indexes)
+    #goals_allocation = [(robot.goalX, robot.goalY) for robot in robots]
+    
+    # Update goals
+    for j in range(0, len(indexes)):
+        robot_index = indexes[j][1]
+        robots[robot_index].goalX.append(stations[next_tasks_to_do[indexes[j][0]]].x)
+        robots[robot_index].goalY.append(stations[next_tasks_to_do[indexes[j][0]]].y)
+        robots[robot_index].goalX.append(stations[tasks_to_do[indexes[j][0]]].x)
+        robots[robot_index].goalY.append(stations[tasks_to_do[indexes[j][0]]].y)
+
+        robots[robot_index].state = 1
+        print(robots[robot_index].name, 'is taking the task', j)
+
+    goals_allocation = [(robot.goalX[-1], robot.goalY[-1]) for robot in robots]
+    #print('goals', goals_allocation)
+    return goals_allocation
+
+
+
+
+
+def get_smallest_path_len_without_change_dir(robots):
+    '''
+    Among all robots, the the length of the shorterst path without a change in direction
+    '''
+
+    idx = max([len(robot.path) for robot in robots])
+    print(idx)
+
+    for robot in robots:
+        for i in range(0, len(robot.path)-1):
+            # If there is a change of direction in the path
+            if abs(robot.path[i][0]- robot.path[i+1][0]) < EPSILON or abs(robot.path[i][1] -robot.path[i+1][1]) < EPSILON:
+                # keep the index of the first change in direction among all paths
+                idx = min(i,idx)
+                print('Smaller Change of dir at index:')
+                print(idx)
+                break # check path of next robot
+
+    print('final idx')
+    print(idx)
+                 
+    return idx
+
+
+
+def check_goal(robot, goals):
+    '''
+    Check if goal is reached, if it's the case, remove from goal list
+    '''
+
+    print(robot.name, 'reached its goal')
+
+    # remove the goal from the robot
+    if len(robot.goalX) > 1:
+        robot.goalX.pop(-1)
+        robot.goalY.pop(-1)
+    # add the new goal in goals_allocation
+    goals[robot.id]=(robot.goalX[-1], robot.goalY[-1])
+    print('new goal for', robot.name, 'is', robot.goalX[-1], robot.goalY[-1])
+
+
+def check_checkpoint(current_robot, robots):
+    '''
+    Check if checkpoint is reached, i.e. if the robot is at a certain distance radius from the checkpoint
+    '''
+    
+    for robot in robots:
+        print(robot.name)
+        print('distance to goal X', abs(robot.posX_gazebo-robot.goal_tempX))
+        print('distance to goal Y', abs(robot.posY_gazebo-robot.goal_tempY))
+        if (abs(robot.posX_gazebo-robot.goal_tempX)<0.5 and abs(robot.posY_gazebo-robot.goal_tempY)<0.5) and min(abs(robot.posX_gazebo-robot.goal_tempX), abs(robot.posY_gazebo-robot.goal_tempY))<0.1:
+            print('next checkpoint')
+            robot.state_checkpoint = 0
+        
+
+def send_checkpoint(robots):
+    '''
+    Send next point to reach to each robot if all robots have reached their previous checkpoint
+    '''
+
+    for robot in robots:
+        if robot.state_checkpoint == 1:
+            print('not there yet')
+            return True 
+        
+    for robot in robots:
+        robot.send_goal()
+    return True 
+
+
+def state_system(robots):
+    '''
+    Move to next task if all robot are free
+    '''
+    for robot in robots:
+         if robot.state == 1:
+                return 1
+    return 0
+
+
+def checkpoint_generator(robot):
+    '''
+    Create checkpoint from the path of the robot where the direction changes
+    '''
+    checkpoint = []
+    checkpoint.append(robot.path[0])
+    for i in range(len(robot.path)-2):
+        if  (robot.path[i+1][0]-robot.path[i][0])*(robot.path[i+2][0]-robot.path[i+1][0])+(robot.path[i+1][1]-robot.path[i][1])*(robot.path[i+2][1]-robot.path[i+1][1]) == 0:
+            checkpoint.append(robot.path[i+1])
+    checkpoint.append(robot.path[-1])
+    robot.path.clear()
+    #robot.change_direction()
+    robot.path = checkpoint.copy()
+
+
+def check_boundaries(X, Y, my_map):
+    '''
+    Check if a point is inside  the map
+    '''
+    if X < len(my_map) and Y < len(my_map)[0] and X > 0 and Y > 0:
+        return True
+    else:
+        return False
+    
+
+def create_robots_from_ini_file(file_path):
+    '''
+    Load robot configuration parameters from ini file 
+    '''
+    config = configparser.ConfigParser()
+    config.read(file_path)
+    global_section = config['Global']
+    number_robot = int(global_section['number_robot'])
+    robots = []
+    for i in range(1, number_robot + 1):
+        robot_config = config['Robots'][f'robot{i}']
+        x, y, station_id = map(int, robot_config.split(','))
+        robot = Robot("robot" + str(i) , x, y, station_id)
+        robots.append(robot)
+    return robots
+
+
+def create_stations_from_ini_file(file_path):
+    '''
+    Load station configuration parameters from ini file 
+    '''
+    config = configparser.ConfigParser()
+    config.read(file_path)
+    stations_list = []
+    stations_config = config['Stations']['stations']
+    station_configs = stations_config.split(';')
+    for station_config in station_configs:
+        name, x, y = station_config.split(',')
+        station = Station(name.strip(), int(x), int(y))
+        stations_list.append(station)
+    return stations_list
+          
+def create_all_from_ini_file(file_path):
+    '''
+    Initialize robots and stations
+    '''
+    robots = create_robots_from_ini_file(file_path)
+    stations = create_stations_from_ini_file(file_path)
+    return robots, stations
+
+if __name__ == '__main__':
+    try:
+        goals = []
+        starts = []
+
+        #rospy.init_node('odometry', anonymous=True) #make node 
+        rospy.init_node('agatha_generate_goals', anonymous=True) #make node 
+
+        # init robots and stations with  initial position and name
+        robots, stations = create_all_from_ini_file('configrobots.ini')
+
+        # Accessing robot and station properties:
+        for robot in robots:
+            print(f"Robot {robot.name}: ({robot.posX}, {robot.posY})")
+
+        for station in stations:
+            print(f"Stations {station.name}: ({station.x}, {station.y})")
+
+        # init map
+        my_map = import_mapf_instance('circuit_MAPF.txt')
+        
+        # generate the tasks
+        tasks_to_do, next_tasks_to_do = task_generator(stations)
+        print('task generated')
+        print(tasks_to_do, next_tasks_to_do)
+        
+        # task allocation
+        goals = task_allocation(stations, tasks_to_do, next_tasks_to_do )
+        print('Goals allocated', goals)
+        #print([(robot.name, robot.goalX, robot.goalY, '................') for robot in robots])
+        
+        # Find paths
+        starts = [(robot.posX, robot.posY) for robot in robots]
+        print('Current position: ', starts)
+        cbs = CBSSolver(my_map, starts, goals)
+        paths = cbs.find_solution('--disjoint')
+
+        
+        # Update paths
+
+        for robot in robots:
+            robot.path = paths[robots.index(robot)]
+            #print(robot.name, robot.path)
+    
+        print('Paths updated') 
+
+        # Change frame MAPF 2 Gazebo (for path)
+        robots[0].base_MAPF2Gazebo()
+        robots[1].base_MAPF2Gazebo()
+        robots[2].base_MAPF2Gazebo()
+        robots[3].base_MAPF2Gazebo()
+        robots[4].base_MAPF2Gazebo()
+
+
+        # Get position (Gazebo frame)
+        rospy.Subscriber('/robot1/odom',Odometry,robots[0].Position)
+        rospy.Subscriber('/robot2/odom',Odometry,robots[1].Position)
+        rospy.Subscriber('/robot3/odom',Odometry,robots[2].Position)
+        rospy.Subscriber('/robot4/odom',Odometry,robots[3].Position)
+        rospy.Subscriber('/robot5/odom',Odometry,robots[4].Position)
+
+    
+        # a checker 
+        robots[0].goal_tempX = robots[0].posX_gazebo
+        robots[0].goal_tempY = robots[0].posY_gazebo
+        robots[1].goal_tempX = robots[1].posX_gazebo
+        robots[1].goal_tempY = robots[1].posY_gazebo
+        robots[2].goal_tempX = robots[2].posX_gazebo
+        robots[2].goal_tempY = robots[2].posY_gazebo
+        robots[3].goal_tempX = robots[3].posX_gazebo
+        robots[3].goal_tempY = robots[3].posY_gazebo
+        robots[4].goal_tempX = robots[4].posX_gazebo
+        robots[4].goal_tempY = robots[4].posY_gazebo
+
+
+
+        while True:
+        
+            print(' ')
+            print('..................................................................')
+
+            state_var = state_system(robots)
+
+            if state_var == 0:
+                
+                # Update goals
+                check_goal(robots[0], goals)
+                check_goal(robots[1], goals)
+                check_goal(robots[2], goals)
+                check_goal(robots[3], goals)
+                check_goal(robots[4], goals)
+
+                # Compute the new paths
+                # 1. Update MAPF position
+                robots[0].base_Gazebo2MAPF()
+                robots[1].base_Gazebo2MAPF()
+                robots[2].base_Gazebo2MAPF()
+                robots[3].base_Gazebo2MAPF()
+                robots[4].base_Gazebo2MAPF()
+    
+                # 2. Get the new starts
+                starts = [(robot.posX, robot.posY) for robot in robots]
+                print('Current position: ', starts)
+                # 3. Compute the new paths
+                cbs = CBSSolver(my_map, starts, goals)
+                paths = cbs.find_solution('--disjoint')
+                # 4. Update the paths
+                robots[0].path = paths[0] 
+                robots[1].path = paths[1]
+                robots[2].path = paths[2]
+                robots[3].path = paths[3]
+                robots[4].path = paths[4]
+     
+                # 5. Change paths frame to Gazebo coordinates
+                robots[0].base_MAPF2Gazebo()
+                robots[1].base_MAPF2Gazebo()
+                robots[2].base_MAPF2Gazebo()
+                robots[3].base_MAPF2Gazebo()
+                robots[4].base_MAPF2Gazebo()
+                print('Paths updated')
+
+
+            # check if checkpoint is reached
+            
+            check_checkpoint(robots[0], robots)
+
+            send_checkpoint(robots)
+
+            get_smallest_path_len_without_change_dir(robots)
+      
+      
+            # Wait 1 sec
+            rospy.sleep(1)
+            
+            
+        rospy.spin()
+       
+            
+
+        
+    except rospy.ROSInterruptException:
+        pass
+
+
+
--- src/edy_controller_node_hardcoded.py
+++ src/edy_controller_node_hardcoded.py
@@ -7,19 +7,24 @@ import math
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist, PoseWithCovariance
 from nav_msgs.msg import Odometry
-from move_base_msgs.msg import MoveBaseGoal
 import tf
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+# TODO: get from fleet manager
+# Target path nodes list
+path = [[-3.18,-2], [0,-0.3]]
 
-#TODO: remove
-global tot_error
+global tot_error,x,y,theta,v,w
 tot_error = 0.0
+x = -2.0
+y = 4.0
+theta = 0.0
+v = 0.0
+w = 0.0
 
 
-# for saving error plots and check if trajectory diverges from a straight line
 error_plots = True
 vel_errors = []
 pos_errors = []
@@ -28,7 +33,7 @@ current_dir = os.getcwd()
 
 class DiffDriveRobot:
 
-    def __init__(self, robot_name = 'edymobile', init_pos = [-3.18,-3.595], init_heading = 0.0, init_vel_cmd = [0.0, 0.0], v_cst = 0.2):
+    def __init__(self, robot_name = 'edymobile', init_pos = [-3.18,-3.595], init_heading = 0.0, init_vel_cmd = [0.0, 0.0], v_cst = 0.1):
         self.robot_name = robot_name
         # Pose
         self.x = init_pos[0]
@@ -40,18 +45,12 @@ class DiffDriveRobot:
         self.cmd_vel.angular.z = init_vel_cmd[1]
         self.v_desired = v_cst
         # Position goal
-        self.goal = MoveBaseGoal()
-        #goal.target_pose.header.frame_id = "map"
-        #goal.target_pose.pose.orientation.w = 1.0
-        #goal.target_pose.pose.position.x = self.path_gazebo[0][0]
-        #goal.target_pose.pose.position.y = self.path_gazebo[0][1]
-        # TODO: modify to only use the movebase goal format
         self.target_x = None
         self.target_y = None
         self.goal_reached = False
         self.dist_to_goal = None
         # Control gains
-        self.Ka = 0.1
+        self.Ka = 0.2
         self.Kv = 0.2
         self.ki = 0.0
         self.kd = 0.001
@@ -72,23 +71,16 @@ class DiffDriveRobot:
         # Calculate the linear error
         self.dist_to_goal = math.sqrt((self.target_x - self.x)**2 + (self.target_y - self.y)**2)
 
-        rospy.loginfo(self.robot_name+': New x target: '+ str(self.target_x))
-        rospy.loginfo(self.robot_name+': New y target: '+str(self.target_y))
-        rospy.loginfo(self.robot_name+': Current x: '+str(self.x))
-        rospy.loginfo(self.robot_name+': Current y: '+str(self.y))
-        rospy.loginfo(self.robot_name+': distance to goal: '+str(self.dist_to_goal))
+        rospy.loginfo('distance to goal')
+        rospy.loginfo(self.dist_to_goal)
 
-        #use same condition as fleet manager
-        if abs(self.target_x - self.x)>0.5 or abs(self.target_y - self.y)>0.5 or min(abs(self.target_y - self.y), abs(self.target_x - self.x))>0.1: #self.dist_to_goal > 0.4:
-
+        if self.dist_to_goal > 0.1:
             # Calculate the heading to the target
             heading = math.atan2(self.target_y - self.y, self.target_x - self.x)
 
+
             # Calculate the angular error
             alpha = heading - self.theta
-
-            rospy.loginfo(self.robot_name+': heading to goal '+ str(heading))
-            rospy.loginfo(self.robot_name+': theta robot in map '+ str(self.theta))
             
             # Normalize the angular error to the range [-pi, pi]
             if alpha > math.pi:
@@ -97,7 +89,8 @@ class DiffDriveRobot:
                 alpha += 2*math.pi
 
 
-            rospy.loginfo(self.robot_name+': angular error: '+ str(alpha))
+            rospy.loginfo('angular error')
+            rospy.loginfo(alpha)
 
             tot_error += alpha
 
@@ -108,11 +101,10 @@ class DiffDriveRobot:
                 v_des = self.v_desired
             
             # Calculate the control inputs
-            # TODO: get true vel from robot data instead of vel command
-            v_error = v_des - self.cmd_vel.linear.x 
+            v_error = v_des - self.cmd_vel.linear.x #TODO: get true vel from robot data instead of vel command
 
-            #rospy.loginfo('vel error')
-            #rospy.loginfo(v_error)
+            rospy.loginfo('vel error')
+            rospy.loginfo(v_error)
             '''
             if error_plots:
                 vel_errors.append(v_error)
@@ -121,29 +113,26 @@ class DiffDriveRobot:
                 pos_errors.append(arr[min_idx])
             '''
                 
-            change_dir = (abs(alpha)>0.05*math.pi/2 and abs(alpha)<0.8*math.pi)
-            #if not change_dir and abs(alpha)>math.pi/2: #move backwards if goal is behind
-            #    alpha = math.pi-alpha
+            change_dir = (abs(alpha)>0.05*math.pi/2 and abs(alpha)<0.8*math.pi/2)
 
-            #rospy.loginfo('change dir?')
-            rospy.loginfo(self.robot_name+': cond1 ? '+str(abs(alpha)>0.05*math.pi/2))
-            rospy.loginfo(self.robot_name+': cond2 ? '+str(abs(alpha)<0.8*math.pi/2))
-            rospy.loginfo(self.robot_name+': change dir ? '+str(change_dir))
+            rospy.loginfo('change dir?')
+            rospy.loginfo(abs(alpha)>0.05*math.pi/2)
+            rospy.loginfo(abs(alpha)<0.8*math.pi/2)
+            rospy.loginfo(change_dir)
 
-            self.cmd_vel.linear.x = (v_des + self.Kv*v_error)*(not change_dir) #+ change_dir*np.sign(v_des)*self.Kv/100 #only apply linear vel if aligned enough with target to avoid avershoot of trajectory at changes in direction
-            self.cmd_vel.angular.z = -self.Ka*(alpha+4*np.sign(alpha)*change_dir) + self.ki*tot_error #angular correction, turn more if change in direction
+            self.cmd_vel.linear.x = (v_des + self.Kv*v_error)*(not change_dir) #only apply linear vel if aligned enough with target to avoid avershoot of trajectory at changes in direction
+            self.cmd_vel.angular.z = -self.Ka*(alpha+2*change_dir) + self.ki*tot_error #angular correction, turn more if change in direction
             
-            #rospy.loginfo('in compute vel function')
-            #rospy.loginfo(self.cmd_vel.linear.x)
+            rospy.loginfo('in compute vel function')
+            rospy.loginfo(self.cmd_vel.linear.x)
 
             # Adjust the angular velocity to maintain approximately constant linear velocity
             if self.dist_to_goal < 0.5:
-            #    rospy.loginfo('Angular vel adjustment')
                 self.cmd_vel.angular.z = self.cmd_vel.angular.z*(self.dist_to_goal/0.5)
 
             
         else:
-            rospy.loginfo(self.robot_name+': Goal reached')
+            rospy.loginfo('Goal reached')
             self.goal_reached = True
             self.set_cmd_vel(0,0)
             tot_error = 0
@@ -161,12 +150,6 @@ class DiffDriveRobot:
         self.y = odom_msg.pose.pose.position.y
         (roll, pitch, self.theta) = tf.transformations.euler_from_quaternion([odom_msg.pose.pose.orientation.x, odom_msg.pose.pose.orientation.y, odom_msg.pose.pose.orientation.z, odom_msg.pose.pose.orientation.w])
 
-    def goalCallBack(self, moveBaseGoal_msg):
-        self.goal = moveBaseGoal_msg
-
-        #test:#TODO: directly use goal in functions
-        self.target_x = self.goal.target_pose.pose.position.x
-        self.target_y = self.goal.target_pose.pose.position.y
 
 
 
@@ -176,36 +159,46 @@ class DiffDriveRobot:
 
 def talker():
 
+    global target_id, path
+
     # Initialize robot
     Edymobile = DiffDriveRobot()  
+
     Edymobile.robot_name = rospy.get_namespace()
+    #rospy.loginfo(Edymobile.robot_name)
 
     rospy.init_node('edy_controller_node', anonymous=True)
 
-    # ################# ROS Subscribers ################# 
-    # Subscribe to robot odometry information and update pose
+    # Subscribe to robot odometry information
     rospy.Subscriber(Edymobile.robot_name+'odom', Odometry, Edymobile.poseCallBack)
-    # Subscribe to goal information from fleet manager: takes next checkpoint in path as goal and update robot goal
-    rospy.Subscriber(Edymobile.robot_name+'move_base/goal', MoveBaseGoal, Edymobile.goalCallBack)
 
-    # ################# ROS Publishers ###################
     # Publish Diff Drive Twist message velocity command
     vel_pub = rospy.Publisher(Edymobile.robot_name+'cmd_vel', Twist, queue_size=10)
     
-    rate = rospy.Rate(10) # 10Hz
+    rate = rospy.Rate(10) # 10hz
 
-    while not rospy.is_shutdown():
-        # If goals have not beed received yet, do not move
-        if  Edymobile.target_x is None or Edymobile.target_y is None: 
-            Edymobile.set_cmd_vel(0, 0)
-        else:
-            Edymobile.compute_vel()
 
+    # TODO: 
+    Edymobile.set_target(path[0][0], path[0][1])
+
+    while not rospy.is_shutdown() and Edymobile.target_x is not None and Edymobile.target_y is not None:
+        Edymobile.compute_vel()
         vel_pub.publish(Edymobile.cmd_vel)
 
-        rospy.loginfo(Edymobile.robot_name+' Velocity command')
+        rospy.loginfo(Edymobile.robot_name+'odom')
         rospy.loginfo(Edymobile.cmd_vel)
+        
 
+
+        if Edymobile.goal_reached == True:
+            rospy.loginfo('Goal reached')
+            target_id +=1
+            if target_id < len(path):
+                Edymobile.set_target(path[target_id][0], path[target_id][1])
+                Edymobile.goal_reached = False
+            else:
+                rospy.loginfo('Final goal reached')
+                break
         rate.sleep()
 
 if __name__ == '__main__':
