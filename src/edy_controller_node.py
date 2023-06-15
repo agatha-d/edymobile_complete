corrected change in direction error which lead the robot to turn towards the side of longest rotation

src/edy_controller_node.py
@@ -14,19 +14,15 @@ import matplotlib.pyplot as plt
 import os
 
 
-#TODO: remove
-global tot_error
-tot_error = 0.0
 
-VERBOSE = True
-
+VERBOSE = False
 DIST_THRESH = 0.3
 
 
 
 class DiffDriveRobot:
 
-    def __init__(self, robot_name = 'edymobile', init_pos = [-3.18,-3.595], init_heading = 0.0, init_vel_cmd = [0.0, 0.0], v_cst = 0.2):
+    def __init__(self, robot_name = 'edymobile', init_pos = [-3.18,-3.595], init_heading = 0.0, init_vel_cmd = [0.0, 0.0], v_cst = 0.1):
         self.robot_name = robot_name
         # Pose
         self.x = init_pos[0]
@@ -47,8 +43,9 @@ class DiffDriveRobot:
         # Control gains
         self.Ka = 0.1
         self.Kv = 0.2
-        self.ki = 0.0001
+        self.ki = 0.00008
         self.kd = 0.001
+        self.tot_error = 0.0
 
     def set_target(self, target_x, target_y):
         self.target_x = target_x
@@ -61,8 +58,6 @@ class DiffDriveRobot:
 
     def compute_vel(self):
 
-        global tot_error
-
         # Calculate the linear error
         self.dist_to_goal = math.sqrt((self.target_x - self.x)**2 + (self.target_y - self.y)**2)
 
@@ -71,11 +66,9 @@ class DiffDriveRobot:
             rospy.loginfo(self.robot_name+': New x target: '+ str(self.target_x))
             rospy.loginfo(self.robot_name+': Current y: '+str(self.y))
             rospy.loginfo(self.robot_name+': New y target: '+str(self.target_y))
-            
-            #rospy.loginfo(self.robot_name+': distance to goal: '+str(self.dist_to_goal))
 
-        # use same condition as fleet manager
-        if abs(self.target_x - self.x)>DIST_THRESH or abs(self.target_y - self.y)>DIST_THRESH or min(abs(self.target_y - self.y), abs(self.target_x - self.x))>0.2: #self.dist_to_goal > 0.4:
+        # must use same condition as fleet manager, otherwise might remain stuck
+        if abs(self.target_x - self.x)>DIST_THRESH or abs(self.target_y - self.y)>DIST_THRESH or min(abs(self.target_y - self.y), abs(self.target_x - self.x))>0.1: #self.dist_to_goal > 0.4:
 
             # Calculate the heading to the target
             heading = math.atan2(self.target_y - self.y, self.target_x - self.x)
@@ -86,20 +79,26 @@ class DiffDriveRobot:
             if VERBOSE:
                 rospy.loginfo(self.robot_name+': heading to goal '+ str(heading))
                 rospy.loginfo(self.robot_name+': theta robot in map '+ str(self.theta))
+
             
             # Normalize the angular error to the range [-pi, pi]
-            if alpha > math.pi:
+            if abs(alpha) > math.pi:
                 alpha -= 2*math.pi
             elif alpha < -math.pi:
                 alpha += 2*math.pi
+            
 
             if VERBOSE:
                 rospy.loginfo(self.robot_name+': angular error: '+ str(alpha))
 
-            tot_error += alpha
+            self.tot_error += alpha
+
+            rospy.loginfo(self.robot_name+': heading to goal '+ str(heading))
+            rospy.loginfo(self.robot_name+': theta robot in map '+ str(self.theta))
+            rospy.loginfo(self.robot_name+': angular error: '+ str(alpha))
 
             # Move backwards if the goal is behind the robot
-            if abs(alpha)>0.8*math.pi:
+            if abs(alpha)>0.5*math.pi:
                 v_des = -self.v_desired
                 rospy.loginfo(self.robot_name+' Should move backwards')
             else:
@@ -115,8 +114,8 @@ class DiffDriveRobot:
             if VERBOSE:
                 rospy.loginfo(self.robot_name+': change dir ? '+str(change_dir))
 
-            self.cmd_vel.linear.x = (v_des + self.Kv*v_error)*(not change_dir) #+ change_dir*np.sign(v_des)*self.Kv/100 #only apply linear vel if aligned enough with target to avoid avershoot of trajectory at changes in direction
-            self.cmd_vel.angular.z = -self.Ka*(alpha+4*np.sign(alpha)*change_dir)*np.sign(v_des) #+ self.ki*tot_error #angular correction, turn more if change in direction
+            self.cmd_vel.linear.x = (v_des + self.Kv*v_error)*(not change_dir) # #only apply linear vel if aligned enough with target to avoid avershoot of trajectory at changes in direction
+            self.cmd_vel.angular.z = -self.Ka*(alpha+4*np.sign(alpha)*change_dir)*np.sign(v_des) #- self.ki*self.tot_error #angular correction, turn more if change in direction
             
 
             # Adjust the angular velocity to maintain approximately constant linear velocity
@@ -125,23 +124,28 @@ class DiffDriveRobot:
 
             
         else:
+            # stop is the goal has been reached
             if VERBOSE:
                 rospy.loginfo(self.robot_name+': Goal reached')
+            rospy.loginfo(self.robot_name+': Goal reached')
             self.goal_reached = True
             self.set_cmd_vel(0,0)
-            #self.cmd_vel.linear.x = 0 #should behave the same way
-            #self.cmd_vel.angular.z = 0
-            tot_error = 0
+            self.tot_error = 0
 
     def poseCallBack(self, odom_msg):
+        '''
+        Get pose information from robot odometry topic
+        '''
         self.x = odom_msg.pose.pose.position.x
         self.y = odom_msg.pose.pose.position.y
         (roll, pitch, self.theta) = tf.transformations.euler_from_quaternion([odom_msg.pose.pose.orientation.x, odom_msg.pose.pose.orientation.y, odom_msg.pose.pose.orientation.z, odom_msg.pose.pose.orientation.w])
 
     def goalCallBack(self, moveBaseGoal_msg):
+        '''
+        Get next goal sent by fleet manager
+        '''
         self.goal = moveBaseGoal_msg
-
-        #test:#TODO: directly use goal in functions
+        #TODO: directly use goal in functions
         self.target_x = self.goal.target_pose.pose.position.x
         self.target_y = self.goal.target_pose.pose.position.y
 
@@ -151,12 +155,13 @@ class DiffDriveRobot:
 
 
 
-def talker():
+def controller_node():
 
     # Initialize robot
     Edymobile = DiffDriveRobot()  
     Edymobile.robot_name = rospy.get_namespace()
 
+    # Initialize ROS node
     rospy.init_node('edy_controller_node', anonymous=True)
 
     # ################# ROS Subscribers ################# 
@@ -188,7 +193,7 @@ def talker():
 
 if __name__ == '__main__':
     try:
-        talker()
+        controller_node()
     except rospy.ROSInterruptException:
         rospy.loginfo('fail')
         pass